f26f08648122dcaa903e5493220b646016a09cf3
aviladcarlos <aviladcarlos@gmail.com>
2026-09-24 15:18:13 -0500

    Added tracing option "on" and "retain-on-failure""

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,7 +25,35 @@ def browser_instance(playwright :Playwright, request):
         browser = playwright.chromium.launch(channel="msedge", headless=False)
 
     context = browser.new_context()
+    tracing_option = request.config.getoption("--tracing")
+
+    if tracing_option == "on" or "retain-on-failure":
+        context.tracing.start(screenshots=True, snapshots=True, sources=True)
+
     page = context.new_page()
     yield page
+
+    if tracing_option == "on" and hasattr(request.node, "test_failed"):
+        test_name = request.path.stem
+        context.tracing.stop(path=f"../test-results/{test_name}_{browser_name}_failed_trace.zip")
+    elif tracing_option == "on":
+        test_name = request.path.stem
+        context.tracing.stop(path=f"../test-results/{test_name}_{browser_name}_trace.zip")
+
+
+    if tracing_option == "retain-on-failure" and hasattr(request.node, "test_failed"):
+        test_name = request.path.stem
+        context.tracing.stop(path=f"../test-results/{test_name}_{browser_name}_failed_trace.zip")
+    else:
+        context.tracing.stop()
+
     context.close()
     browser.close()
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item: pytest.Item, call: pytest.CallInfo):
+    outcome = yield
+    report = outcome.get_result()
+
+    if report.when == "call" and report.failed:
+        item.test_failed = True
